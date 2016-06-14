b50a3209e5e715750eb7cdfe54b5d3bba901361a
castillo55 <israel_co@hotmail.com>
2016-06-14 12:19:40 -0500

    Carrusel
    Nuevo

--- index.php
+++ index.php
@@ -1,56 +1,44 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<title>Shop Men's and Women's Clothing | Express</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
+	<title>Shop Men's and Women's Clothing | Express</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet"
 	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script
+	<script
 	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<script
+	<script
 	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script
+	<script
 	src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.1/holder.min.js"></script>
-<link rel="stylesheet" type="text/css" href="./css/custom_css.css"
+
+	<link rel="stylesheet" type="text/css" href="./css/custom_css.css"
 	media="screen" />
-<link rel="stylesheet" type="text/css" href="./css/footer.css"
+	<link rel="stylesheet" type="text/css" href="./css/footer.css"
 	media="screen" />
-<link rel="stylesheet" type="text/css" href="./css/mediasFooter.css" media="screen" />
-<link rel="stylesheet" type="text/css" href="./css/carrusel.css" media="screen" />
-<script type="text/javascript">
-	$('#carousel-example-generic').on(
-			'slid.bs.carousel',
-			function() {
-				$(
-						".item.active:nth-child("
-								+ ($(".carousel-inner .item").length - 1)
-								+ ") + .item").insertBefore(
-						$(".item:first-child"));
-				$(".item.active:last-child").insertBefore(
-						$(".item:first-child"));
-			});
-</script>
+	<link rel="stylesheet" type="text/css" href="./css/mediasFooter.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="./css/carrusel.css" media="screen" />
 </head>
 <body>
 	<!-- Inicia el contenido del home -->
 	<div class="container-element section-component">
 		<div class="row layout-element-1">
 			<img class="single-img"
-				src="/test1/imagenes/express-women-men-shorts-hp.jpg">
+			src="/test1/imagenes/express-women-men-shorts-hp.jpg">
 		</div>
 		<div class="row layout-element-1">
 			<div class="col-custom">
 				<img class="single-img"
-					src="/test1/imagenes/epress-women-skirts-hp.jpg">
+				src="/test1/imagenes/epress-women-skirts-hp.jpg">
 			</div>
 			<div class="col-center">
 				<img class="single-img"
-					src="/test1/imagenes/epress-men-casualshirts-hp.jpg">
+				src="/test1/imagenes/epress-men-casualshirts-hp.jpg">
 			</div>
 			<div class="col-custom">
 				<img class="single-img"
-					src="/test1/imagenes/epress-women-one-eleven-hp.jpg">
+				src="/test1/imagenes/epress-women-one-eleven-hp.jpg">
 			</div>
 		</div>
 		<div class="row layout-space">
@@ -142,73 +130,64 @@
 					<span>@EXPRESS</span>
 					<span>#EXPRESSLIFE</span>
 				</h3>
-				<div class="btn-header">
-					<a class="btn btn-link" href="">UPLOAD YOUR PIC</a>
-					<a class="view-galery">VIEW GALERY</a>
-				</div>
 			</div>
 			<div id="carousel-example-generic" class="carousel slide"
-				data-ride="carousel" style="max-width: 1277px;">
-				<!-- Indicators -->
+			data-ride="carousel" style="max-width: 1277px;">
+				<!-- Indicators 
 				<ol class="carousel-indicators">
 					<li data-target="#carousel-example-generic" data-slide-to="0"
 						class="active"></li>
 					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
 					<li data-target="#carousel-example-generic" data-slide-to="2"></li>
 				</ol>
-				<!-- Wrapper for slides -->
-				<div class="carousel-inner" role="listbox">
-					<div class="item active">
-						<img src="/test1/imagenes/-dp6EiBvQEr6o_G7O1P8f5RtZPsNl0JzfvRBWB-oWj4=.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/b63ae149-f094-438c-8dc4-f023b4cf8497.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/1648ff1b-7eb4-4c2f-bffa-99d6b19f57c2.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/30019ae1-8149-4c31-a991-ca6e80937b09.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/GSWQyKN4KkdBkkxRPJm2Mi2p1HzYNP2KlXdcPUThgUI=.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/a59525c5-20ca-4b84-8893-4dc493cc6338.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/ab0980bd-e3a7-44a5-b682-dce15f74abbd.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/5nK9AyKO0NbSWXONQCQdwtPqCkFNkjE3BN9W-QZenfE=.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/20coXCFvz-BrT9TwwL8MEZ8rL4FsgzA3Tcj91YciV1A=.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/5e6bf62e-d1a6-4b92-a4d0-c3be23a815db.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/5e6bf62e-d1a6-4b92-a4d0-c3be23a815db.jpg">
-					</div>
-					<div class="item">
-						<img src="/test1/imagenes/5e6bf62e-d1a6-4b92-a4d0-c3be23a815db.jpg">
-					</div>
+			-->
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="/test1/imagenes/-dp6EiBvQEr6o_G7O1P8f5RtZPsNl0JzfvRBWB-oWj4=.jpg">
 				</div>
-
-				<!-- Controls -->
-				<a class="left carousel-control" href="#carousel-example-generic"
-					role="button" data-slide="prev"> <span
-					class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a> <a class="right carousel-control" href="#carousel-example-generic"
-					role="button" data-slide="next"> <span
-					class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
+				<div class="item">
+					<img src="/test1/imagenes/b63ae149-f094-438c-8dc4-f023b4cf8497.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/1648ff1b-7eb4-4c2f-bffa-99d6b19f57c2.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/30019ae1-8149-4c31-a991-ca6e80937b09.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/GSWQyKN4KkdBkkxRPJm2Mi2p1HzYNP2KlXdcPUThgUI=.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/a59525c5-20ca-4b84-8893-4dc493cc6338.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/ab0980bd-e3a7-44a5-b682-dce15f74abbd.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/5nK9AyKO0NbSWXONQCQdwtPqCkFNkjE3BN9W-QZenfE=.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/20coXCFvz-BrT9TwwL8MEZ8rL4FsgzA3Tcj91YciV1A=.jpg">
+				</div>
+				<div class="item">
+					<img src="/test1/imagenes/5e6bf62e-d1a6-4b92-a4d0-c3be23a815db.jpg">
+				</div>
 			</div>
-		</div>
-	</div>
-	<?php include("footer.html")?>
+
+			<!-- Controls -->
+			<a class="left carousel-control" href="#carousel-example-generic"
+			role="button" data-slide="prev"> <span
+			class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a> <a class="right carousel-control" href="#carousel-example-generic"
+		role="button" data-slide="next"> <span
+		class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+</div>
+</div>
+<?php include("footer.html")?>
 </body>
 </html>
