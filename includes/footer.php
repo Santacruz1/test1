6555eb84cfeacb3886732675f84006a38d6cd1ed
Santacruz1 <edgar_27_41@hotmail.com>
2016-06-17 12:26:38 -0500

    agregando includes
    se agrego la carpeta includes donde viene el header y footer

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+
+
+</div> <!-- container -->   
+
+<footer>
+
+   <div class="container">
+      
+   ----------footer
+
+   </div>
+
+</footer>
+
+<script src="./js/jquery-1.12.2.js"></script>  
+ 
+
+</body>
+</html>
+
+
+
