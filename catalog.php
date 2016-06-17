2c443d8b6b8c9313cc566edc20ccc48e1196a166
Santacruz1 <edgar_27_41@hotmail.com>
2016-06-17 12:20:30 -0500

    Catalogo
    Se sube el catalogo de productos de mujeres con sus respectivas hojas de
    estilo

---
+++ catalog.php
@@ -0,0 +1,192 @@
+<?php include('./includes/header.php');  ?> 
+
+<div class="row">
+	<div class="col-xs-12"> 
+		<ul class="navegacion">
+			<li><a href="#">Mujeres</a></li>
+			/
+			<li><span>Mujeres</span></li>
+		</ul> 
+	</div>
+
+	<div class="col-md-6">
+		<h1>Womens New Arrivals</h1>
+	</div>
+
+	<div class="col-md-6">
+
+	</div>
+
+</div>
+
+
+
+
+
+<div class="row">
+<div class="col-xs-5 col-sm-5 col-md-5">
+	<img class="img-responsive" src="images/catalog/índice.jpg">
+	
+	<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5"> 
+	<img class="img-responsive" src="images/catalog/2.jpg">
+	<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"> 2 colors</span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5"> 
+	<img class="img-responsive" src="images/catalog/83_821.jpg">
+
+	<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5"> 
+	<img class="img-responsive" src="images/catalog/74_792.jpg">
+
+	<ul class="product-info">
+		<li class="price">
+			<span class="regular-price">$69.90</span>
+			<span class="discount">$35.94</span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+
+		<li class="promo">
+		<font style="font-weight:normal" color="BLACK">Now 40% Off!</font><br>
+		<font style="font-weight:normal" color="BLACK">Price Reflects Discount</font>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5">
+	<img class="img-responsive" src="images/catalog/74_936.jpg"> 
+
+	<ul class="product-info">
+		<li class="price">
+			<span class="regular-price">$69.90</span>
+			<span class="discount">$35.94</span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+
+		<li class="promo">
+		<font style="font-weight:normal" color="BLACK">Now 40% Off!</font><br>
+		<font style="font-weight:normal" color="BLACK">Price Reflects Discount</font>
+		</li>
+	</ul>
+</div>
+
+<!--        2             -->
+<div class="col-xs-5 col-sm-5 col-md-5">
+	<img class="img-responsive" src="images/catalog/86_630.jpg">
+
+		<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5"> 
+	<img class="img-responsive" src="images/catalog/74_234.jpg">
+
+		<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5"> 
+	<img class="img-responsive" src="images/catalog/83_737.jpg">
+
+		<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5"> 
+	<img class="img-responsive" src="images/catalog/74_933.jpg">
+
+		<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+<div class="col-xs-5 col-sm-5 col-md-5">
+	<img class="img-responsive" src="images/catalog/87_870.jpg"> 
+
+		<ul class="product-info">
+		<li class="price">
+			<span>$69.90</span>
+			<span class="discount"></span>
+			<span class="colors"></span>
+		</li>
+		<li class="description">
+			<a href="#">one shoulder belted asymmetrical maxi dress</a>
+		</li>
+	</ul>
+</div>
+
+</div> <!-- Cierre de class row-->
+
+
+ 
+
+<?php include('./includes/footer.php');  ?> 
