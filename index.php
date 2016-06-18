ab25dd52509540e58764b5dbfad26a7ee630f69f
Iris Cuevas <icuevas@maftec.com.mx>
2016-06-17 20:29:03 -0500

    Express

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<title>Shop Men's and Women's Clothing | Express</title>
+	<title>Express</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet"
@@ -10,7 +10,8 @@
 	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script
 	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	
+	<link href="./css/bootstrap.css" rel="stylesheet">
+    <link href="./css/mediasquery.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="./css/_header.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="./css/custom_css.css"
 	media="screen" />
@@ -18,210 +19,18 @@
 	media="screen" />
 	<link rel="stylesheet" type="text/css" href="./css/mediasFooter.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="./css/carrusel.css" media="screen" />
+	
 
 	<script>
-	$(document).ready(function(){
+		$(document).ready(function(){
 	        $("#myCarousel").carousel("cycle");
-	});
+		});
 	</script>
 
 </head>
 <body>
-	<?php include("header.html")?>
-	<!-- Inicia el contenido del home -->
-	<div class="container-element section-component">
-		<div class="row layout-element-1">
-			<img class="single-img"
-			src="imagenes/express-women-men-shorts-hp.jpg">
-		</div>
-		<div class="row layout-element-1">
-			<div class="col-custom">
-				<img class="single-img"
-				src="imagenes/epress-women-skirts-hp.jpg">
-			</div>
-			<div class="col-center">
-				<img class="single-img"
-				src="imagenes/epress-men-casualshirts-hp.jpg">
-			</div>
-			<div class="col-custom">
-				<img class="single-img"
-				src="imagenes/epress-women-one-eleven-hp.jpg">
-			</div>
-		</div>
-		<div class="row layout-space">
-			<br>
-		</div>
-		<div class="row layout-element-1">
-			<div class="accessory-header">
-				<h2 class="title-accessory">
-					<b> <span>ACCESSORY SHOP</span></b>
-				</h2>
-			</div>
-			<div class="form-accessory">
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/sandals-3.jpg">
-						<div class="footer-title">
-							<span class="span-link"> women's sandals <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/womens-acc.jpg">
-						<div class="footer-title">
-							<span class="span-link"> women's jewelry <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/patches-4.jpg">
-						<div class="footer-title">
-							<span class="span-link"> women's pins & patches <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/womens-handbags.jpg">
-						<div class="footer-title">
-							<span class="span-link"> women's handbags <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/mens-ties.jpg">
-						<div class="footer-title">
-							<span class="span-link"> men's ties <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/mens-flip-flops.jpg">
-						<div class="footer-title">
-							<span class="span-link"> men's shoes <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/mens-socks-2.jpg">
-						<div class="footer-title">
-							<span class="span-link"> men's socks <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-				<div class="col col-accessory">
-					<a href=""> <img class="img-accessory"
-						src="imagenes/mens-underwear.jpg">
-						<div class="footer-title">
-							<span class="span-link"> men's underwear <span
-								class="glyphicon glyphicon-play size"></span>
-							</span>
-						</div>
-					</a>
-				</div>
-			</div>
-		</div>
-		<div class="row layout-element-1">
-			<div class="carrusel-header">
-				<h3>
-					<a href=""><img src="imagenes/Curalate-IG-Icon-Final.png"></a>
-					<span>@EXPRESS</span>
-					<span>#EXPRESSLIFE</span>
-				</h3>
-			</div>
-			<div id="carouse-express" class="carousel slide" data-ride="carousel">
-				<!-- Indicators 
-				<ol class="carousel-indicators">
-					<li data-target="#carouse-express" data-slide-to="0"
-						class="active"></li>
-					<li data-target="#carouse-express" data-slide-to="1"></li>
-					<li data-target="#carouse-express" data-slide-to="2"></li>
-				</ol>
-			-->
-			<!-- Wrapper for slides -->
-			<div id="myCarousel" class="carousel-inner" role="listbox">
-				<div class="item active">
-					<img src="imagenes/-dp6EiBvQEr6o_G7O1P8f5RtZPsNl0JzfvRBWB-oWj4=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/b63ae149-f094-438c-8dc4-f023b4cf8497.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/1648ff1b-7eb4-4c2f-bffa-99d6b19f57c2.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/30019ae1-8149-4c31-a991-ca6e80937b09.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/GSWQyKN4KkdBkkxRPJm2Mi2p1HzYNP2KlXdcPUThgUI=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/a59525c5-20ca-4b84-8893-4dc493cc6338.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/ab0980bd-e3a7-44a5-b682-dce15f74abbd.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/5nK9AyKO0NbSWXONQCQdwtPqCkFNkjE3BN9W-QZenfE=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/20coXCFvz-BrT9TwwL8MEZ8rL4FsgzA3Tcj91YciV1A=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/5e6bf62e-d1a6-4b92-a4d0-c3be23a815db.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/_lLCBdE8N_uU9lAEXWRGGl9rAsve0nh8JK-1kL9PlFo=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/0acbce26-c3fa-4e22-869e-6b344996aa2a.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/b2S3Wdn7gjSEI74sndEUbcibTh4_4ZE1xL1mhoi30IM=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/2f7c5de3-d150-4225-8893-40f0a3e63d6f.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/7nh3NPAc5y1JGqbnvwkEKKRnC1cU-1-nv_mpU07_IMk=.jpg">
-				</div>
-				<div class="item">
-					<img src="imagenes/5yimCIH4i0sU-gCDcKDdthhQX5W3iWkKm0M3NwWeaSs=.jpg">
-				</div>
-			</div>
-
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carouse-express"
-			role="button" data-slide="prev"> <span
-			class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a> <a class="right carousel-control" href="#carouse-express"
-		role="button" data-slide="next"> <span
-		class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
-</div>
-</div>
-</div>
+<?php include("header.html")?>
+<?php include("index.html")?>
 <?php include("footer.html")?>
 </body>
 </html>
